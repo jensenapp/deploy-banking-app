8a6499f3b61f56f3127ab6d19cfa84ffe45ea8bd
jensenapp <chssun@gmail.com>
2025-09-23 21:35:04 +0800

    update AccountController.java

--- src/main/java/net/javaguides/banking/controller/AccountController.java
+++ src/main/java/net/javaguides/banking/controller/AccountController.java
@@ -31,7 +31,7 @@ public class AccountController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('admin') or @accountSecurityService.canCreateAccountFor(authentication,#accountDto.accountHolderName())")
+    @PreAuthorize("hasRole('ADMIN') or @accountSecurityService.canCreateAccountFor(authentication,#accountDto.accountHolderName())")
     public ResponseEntity<AccountDto> addAccount(@Valid @RequestBody AccountDto accountDto) {
 
         AccountDto account = accountService.createAccount(accountDto);
